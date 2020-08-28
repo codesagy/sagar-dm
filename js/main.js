9c73e58464fbee8f353e5bcd637bf3e25650b809
Sagar Adhalge <codesagy@gmail.com>
2020-08-28 12:24:07 +0530

    Updated
    Front end update

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Initialize and add the map
+<script>
+  function initMap() {
+  // The location of Uluru 18.552877,73.822142
+  var uluru = {lat: 18.552877, lng: 73.822142};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+</script>
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
